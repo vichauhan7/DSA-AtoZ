9173ca6ea726e663630a347d48e4b4a07e94470b
vichauhan7 <vichauhan9636@gmail.com>
2026-01-31 17:06:27 +0530

    PATTERN - 2

--- Chapter-1-Basics/1.2-Patterns/pattern1_prac.py
+++ Chapter-1-Basics/1.2-Patterns/pattern1_prac.py
@@ -4,5 +4,12 @@ def print1(n):
             print('*', end='')
         print()
 
+def print2(n):
+    for i in range(n):
+        for j in range(i):
+            print(j + 1, end='')
+        print()
+
 
 print1(4)
+print2(5)
