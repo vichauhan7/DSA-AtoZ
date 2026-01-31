c9a7145344fa085e7a24057ddf8502bc00ab00f5
vichauhan7 <vichauhan9636@gmail.com>
2026-01-31 22:09:28 +0530

    ptr10

--- Chapter-1-Basics/1.2-Patterns/pattern1_prac.py
+++ Chapter-1-Basics/1.2-Patterns/pattern1_prac.py
@@ -16,5 +16,73 @@ def print3(n):
             print(j+1, end='')
         print()
 
+def print4(n):
+    for i in range(n):
+        for j in range(i+1):
+            print(i+1, end='')
+        print()
 
-print3(5)
+def print5(n):
+    for i in range(n):
+        for j in range(n-i):
+            print('*', end='')
+        #n=n-1   
+        print()
+
+def print6(n):
+    for i in range(n):
+        for j in range(n-i):
+            print(j+1, end='')
+        print()
+
+def print7(n):
+    for i in range(n):
+        for j in range(n-i-1):
+            print(' ', end='')
+        for j in range((i*2)+1):
+            print('*', end='')
+        for j in range(n-i-1):
+            print(' ', end='')                        
+        print()
+
+def print8(n):
+    for i in range(n):
+        for j in range(i):
+            print(' ', end='')
+        for j in range((n*2) - (i*2) -1):
+            print('*', end='')
+        for j in range(i):
+            print(' ', end='')                        
+        print()
+
+def print9(n):
+
+    for i in range(n):
+        for j in range(n-i-1):
+            print(' ', end='')
+        for j in range((i*2)+1):
+            print('*', end='')
+        for j in range(n-i-1):
+            print(' ', end='')                        
+        print()
+        
+    for i in range(n):
+        for j in range(i):
+            print(' ', end='')
+        for j in range((n*2) - (i*2) -1):
+            print('*', end='')
+        for j in range(i):
+            print(' ', end='')                        
+        print()
+
+
+def print10(n):
+    for i in range((2*n)-1):
+        star = i + 1
+        if i > n - 1: star = (2*n)-i -1      
+        for j in range(star):
+            print('*', end='')
+        print()
+
+
+print10(5)
