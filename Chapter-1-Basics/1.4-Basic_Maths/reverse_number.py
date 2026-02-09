dbd5635e7fb53b67268d46506ac8cd5704f8d99f
vichauhan7 <vichauhan9636@gmail.com>
2026-02-09 22:34:24 +0530

    upd

--- Chapter-1-Basics/1.4-Basic_Maths/reverse_number.py
+++ Chapter-1-Basics/1.4-Basic_Maths/reverse_number.py
@@ -1,6 +1,42 @@
-class Solution:
-    def reverse(self, x: int) -> int:
-        rev = 0
-        while x > 0 :
-            rev = (x % 10) * 10 + rev
-        return rev
+def reverse(x: int) -> int:
+    minus_ind = 0
+    INT_MAX = 2**31 - 1
+    INT_MIN = -2**31 
+    reverse_num : int = 0
+    if x < 0:
+        minus_ind = 1
+        x = x * -1    
+    while x > 0:
+        reverse_num =  (x % 10 ) + (reverse_num * 10)
+        x = x // 10
+    if minus_ind == 1:
+        reverse_num = reverse_num * -1
+
+    if reverse_num >= INT_MAX  or reverse_num <= INT_MIN:
+        print (0)
+    else:
+        print (reverse_num)
+
+
+
+
+def isPalindrome(x: int) -> bool:
+    if x < 0 :
+        return False
+    dup = x
+    int_max = 2**31 - 1
+    rev = 0
+    while x:
+        digit = x % 10
+        x = x // 10
+        rev = (rev * 10 ) + digit
+    print(rev)
+    if rev >= int_max :
+        return False
+    elif dup == rev:
+        return True
+    else :
+        return False
+
+res = isPalindrome(10)
+print(res)
